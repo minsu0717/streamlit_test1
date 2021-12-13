557a9293bd4a3492208fe19b4ca8e8dc6406723c
minsu0717 <alstnalstnalstnrlaalstn@gmail.com>
2021-12-13 22:38:46 +0900

    test

--- app3.py
+++ app3.py
@@ -2,4 +2,14 @@ import streamlit as st
 import pandas as pd
 
 def main() :
-    pd.read_csv('iris.csv')
+    #  데이터프레임 읽기만 하면 출력을 안한다.
+    df=pd.read_csv('data/iris.csv')
+    st.dataframe(df)
+    
+    species=df['species'].unique()
+    st.text('아이리스 꽃은' + species + '으로 되어있다.')
+    
+    st.write(df.head())
+    
+if __name__ == '__main__':
+    main()
--- data/app2.py
+++ data/app2.py
@@ -5,7 +5,14 @@ import streamlit as st
 # 웹 대시보드 개발 라이브러리인 스트림릿은 main 함수가 잇어야한다.
 
 def main() :
-    pass
+    st.title('Hello Streamlit. 웹 대시보드')
+    st.title('헬로우')
 
 if __name__ =='main' :
+    main()
+
+# 터미널에서 ctrl+c 를 하면 streamlit이 꺼진다
+
+if __name__ == '__main__' :
+    print(__name__)
     main()
